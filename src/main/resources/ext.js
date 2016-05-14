55edf32312df9330a609f9466930b3972f0b8c3a
chinthakaak <chinthakaak@gmail.com>
2016-05-14 20:20:02 +0530

    add ex2

---
+++ src/main/resources/ext.js
@@ -0,0 +1,11 @@
+	function validate() {
+		var x;
+		x = document.getElementById("text").value;
+
+		if (isNaN(x) || x < 1 || x > 10) {
+	        text = "Input not valid";
+	    } else {
+	        text = "Input OK";
+	    }
+	    document.getElementById("demo").innerHTML = text;
+	}
